b14e85d34af936d09a5fd0802f7f8cb954c4ad4e
codemonkey85 <codemonkey85@gmail.com>
2014-05-30 16:06:05 -0400

    Initial commit of project files

---
+++ MultiThreadingTest/frmMain.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace MultiThreadingTest
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.pbProgress = new System.Windows.Forms.ProgressBar();
+            this.tProgress = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnStop.Location = new System.Drawing.Point(155, 12);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 1;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // pbProgress
+            // 
+            this.pbProgress.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pbProgress.Location = new System.Drawing.Point(12, 113);
+            this.pbProgress.Name = "pbProgress";
+            this.pbProgress.Size = new System.Drawing.Size(218, 23);
+            this.pbProgress.TabIndex = 2;
+            // 
+            // tProgress
+            // 
+            this.tProgress.Tick += new System.EventHandler(this.tProgress_Tick);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(242, 148);
+            this.Controls.Add(this.pbProgress);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.MinimumSize = new System.Drawing.Size(245, 114);
+            this.Name = "frmMain";
+            this.Text = "Multithreading Test";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.ProgressBar pbProgress;
+        private System.Windows.Forms.Timer tProgress;
+    }
+}
+
---
+++ MultiThreadingTest/frmMain.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+namespace MultiThreadingTest
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+        private delegate void StartDelegate();
+        private void Start()
+        {
+            if (InvokeRequired)
+            {
+                StartDelegate start = new StartDelegate(Start);
+                Invoke(start, null);
+            }
+            else
+            {
+                tProgress.Start();
+            }
+        }
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            Start();
+        }
+        private delegate void StopDelegate();
+        private void Stop()
+        {
+            if (InvokeRequired)
+            {
+                StopDelegate stop = new StopDelegate(Stop);
+                Invoke(stop, null);
+            }
+            else
+            {
+                tProgress.Stop();
+            }
+        }
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            Stop();
+        }
+        private delegate void tProgressTickDelegate();
+        private void tProgressTick()
+        {
+            if (InvokeRequired)
+            {
+                tProgressTickDelegate tick = new tProgressTickDelegate(tProgressTick);
+                Invoke(tick, null);
+            }
+            else
+            {
+                if (pbProgress.Value == pbProgress.Maximum)
+                {
+                    pbProgress.Value = pbProgress.Minimum;
+                }
+                else
+                {
+                    pbProgress.Value++;
+                }
+            }
+        }
+        private void tProgress_Tick(object sender, EventArgs e)
+        {
+            tProgressTick();
+        }
+    }
+}
